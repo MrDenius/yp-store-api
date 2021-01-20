456be9c3f30f4dfb55e66dec1ab1492d39a007ec
Denius <denisa5700@yandex.ru>
2021-01-20 14:43:40 +1000

    push

--- views/Position/main.js
+++ views/Position/main.js
@@ -1,12 +1,27 @@
 const urlParams = new URLSearchParams(window.location.search);
 const content = document.querySelector("#content");
 
-const init = () => {
+const init = async () => {
 	for (var p of urlParams.entries()) {
 		let value = p[1];
 		switch (p[0]) {
 			case "name":
-				Name(value);
+				Name(value, content.name);
+				break;
+			case "description":
+				Description(value, content.description);
+				break;
+			case "price":
+				Price(value, content.price);
+				break;
+			case "img":
+				Img(value, content.img);
+				break;
+			case "full":
+				await Full();
+				break;
+			case "small":
+				await Small();
 				break;
 			default:
 				break;
@@ -14,9 +29,58 @@ const init = () => {
 	}
 };
 
-const Name = (text) => {
+const StartPay = () => {};
+
+const Full = async () => {
+	document.body.innerHTML = await (await fetch("./full.html")).text();
+
+	content.name = document.querySelector("#name");
+	content.description = document.querySelector("#description");
+	content.price = document.querySelector("#price");
+	content.img = document.querySelector("#img");
+
+	content.price.addEventListener("click", StartPay);
+};
+
+const Small = async () => {
+	document.body.innerHTML = await (await fetch("./small.html")).text();
+
+	content.name = document.querySelector("#name");
+	content.price = document.querySelector("span#price");
+	content.img = document.querySelector("#img");
+
+	content.description = "disable";
+};
+
+const Name = (text, content) => {
+	if (content === "disable") return;
+	content = content || this.content;
 	content.innerHTML = text;
 	content.classList = "name";
+};
+
+const Description = (text, content) => {
+	if (content === "disable") return;
+	content = content || this.content;
+	content.innerHTML = text;
+	content.classList = "description";
+};
+
+const Price = (text, content) => {
+	if (content === "disable") return;
+	content = content || this.content;
+	content.innerHTML = `${text}`;
+	content.classList = "price";
+};
+
+const Img = (url, content) => {
+	if (content === "disable") return;
+	content = content || this.content;
+	const img = document.createElement("img");
+	img.src = url;
+	img.classList = "img";
+	content.appendChild(img);
+	content.classList = "img";
 };
 
 init();
--- views/view.js
+++ views/view.js
@@ -7,8 +7,8 @@ const init = (port) => {
 	const api = async (params) => {
 		console.log(params);
 		const quest = params.q;
-		const item = params.i || "";
-		const id = params.id;
+		const item = params.i;
+		const id = params.id || "60069cf1f8c79a3fb07d8094";
 		params.q = undefined;
 		params.id = undefined;
 		params.i = undefined;
@@ -21,9 +21,23 @@ const init = (port) => {
 
 		switch (item) {
 			case "name":
-				qu += `name=${
-					(await GetPosition("60069cf1f8c79a3fb07d8094")).name
-				}`;
+				qu += `name=${(await GetPosition(id)).name}`;
+				break;
+			case "full":
+				qu += `full=1`;
+				Object.entries(await GetPosition(id)).forEach((p) => {
+					qu += `${p[0]}=`;
+					if (p[1]) qu += p[1];
+					qu += "&";
+				});
+				break;
+			case "small":
+				qu += `small=1`;
+				Object.entries(await GetPosition(id)).forEach((p) => {
+					qu += `${p[0]}=`;
+					if (p[1]) qu += p[1];
+					qu += "&";
+				});
 				break;
 		}
 		if (quest === "position") {
@@ -31,16 +45,27 @@ const init = (port) => {
 				return `views/Position/?${qu}`;
 			}
 		}
+		if (quest === "catalog") {
+		}
 	};
 
+	const AddPosition = (pos) => {
+		const frame = document.createElement("iframe");
+	};
+
+	let _position;
 	const GetPosition = async (id) => {
+		if (_position) {
+			return _position;
+		}
 		const query = `#graphql
         {
             position(id: \"${id}\") {
-              type,
-              name,
-              description,
-              price,
+              	type,
+              	name,
+              	description,
+              	price,
+              	img,
             }
         }`.replace(/(([ \n])|#graphql)*/g, "");
 
@@ -48,7 +73,32 @@ const init = (port) => {
 			await fetch(`${global.address}graphql?query=${query}`)
 		).json();
 		console.log(queryData);
-		return queryData.data.position;
+		_position = queryData.data.position;
+		return _position;
+	};
+
+	let _positionsByType;
+	const GetPositionsByType = async (type) => {
+		if (_positionsByType) {
+			return _positionsByType;
+		}
+		const query = `#graphql
+        {
+            positionsByType(type: \"${type}\") {
+				id,
+              	name,
+              	description,
+              	price,
+              	img,
+            }
+        }`.replace(/(([ \n])|#graphql)*/g, "");
+
+		const queryData = await (
+			await fetch(`${global.address}graphql?query=${query}`)
+		).json();
+		console.log(queryData);
+		_positionsByType = queryData.data.positionsByType;
+		return _positionsByType;
 	};
 
 	return api;
